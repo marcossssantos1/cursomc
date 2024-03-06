511d279ff8a4381bb1eea6411a8c845792f9501b
Marcos Santos <marcossantosmsms604@gmail.com>
2024-03-05 21:10:35 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/marcossantos/cursomc/resources/CategoriaResouce.java
+++ src/main/java/com/marcossantos/cursomc/resources/CategoriaResouce.java
@@ -1,16 +1,26 @@
 package com.marcossantos.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.marcossantos.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResouce {
-	
+
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando perfeitamente!!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 
 }
