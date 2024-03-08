df93bddaa8f662113a951c7045a33fa8a8b5303a
Marcos Santos <marcossantosmsms604@gmail.com>
2024-03-07 21:54:41 -0300

    Estado e Cidade

---
+++ src/main/java/com/marcossantos/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.marcossantos.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcossantos.cursomc.domain.Cidade;
+
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
