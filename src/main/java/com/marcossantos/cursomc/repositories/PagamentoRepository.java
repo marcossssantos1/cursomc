d2c71963748dad5d1089dcdfafc13756d942db68
Marcos Santos <marcossantosmsms604@gmail.com>
2024-03-07 22:18:26 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/marcossantos/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,13 @@
+package com.marcossantos.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcossantos.cursomc.domain.Pagamento;
+
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
